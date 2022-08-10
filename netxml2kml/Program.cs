c3fc536b17eb788a61673bfc5c4634675aad1c77
cuqmbr <dr.juniorf@gmail.com>
2022-08-10 21:03:39 +0300

    feat: add cli options validation

--- netxml2kml/Program.cs
+++ netxml2kml/Program.cs
@@ -2,90 +2,104 @@
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
+using netxml2kml.Data;
 using netxml2kml.Models;
 
 namespace netxml2kml;
 
-static class Program
+class Program
 {
     static async Task<int> Main(string[] args)
     {
+        // Define CLI parser options, commands & handlers
+        
         var inputOption = new Option<FileInfo?>(
             aliases: new[] {"-i", "--input"},
-            description: "The file to be converted to .kml format.");
+            description: "Path to the file to be converted.");
 
         var outputOption = new Option<FileInfo?>(
             aliases: new[] {"-o", "--output"},
             description: "The name of the file to be created.");
 
-        var rootCommand =
-            new RootCommand("netxml2kml – .netxml to .kml converter.");
+        var rootCommand = new RootCommand("netxml2kml – .netxml to .kml converter.");
         rootCommand.AddOption(inputOption);
         rootCommand.AddOption(outputOption);
 
         rootCommand.SetHandler((inputFile, outputFile) =>
             {
-                if (outputFile == null)
+                if (!IsValidArguments(inputFile, ref outputFile,
+                        out string validationError))
                 {
-                    outputFile = new FileInfo($"{inputFile!.Name.Substring(0, inputFile!.Name.Length - inputFile.Extension.Length)}.kml");
-                }
-
-                var serializer = new XmlSerializer(typeof(detectionrun));
-                var detectionRun = (detectionrun?) serializer.Deserialize(XmlReader.Create(inputFile!.OpenRead(), new XmlReaderSettings {DtdProcessing = DtdProcessing.Parse}));
-
-                if (detectionRun == null)
-                {
-                    throw new ArgumentNullException();
+                    Console.WriteLine(validationError);
+                    return;
                 }
                 
-                var kmlString = GenerateKml(detectionRun, inputFile.Name);
+                var serializer = new XmlSerializer(typeof(detectionrun));
+                var detectionRun = (detectionrun?) serializer.Deserialize(XmlReader.Create(inputFile.OpenRead(), new XmlReaderSettings {DtdProcessing = DtdProcessing.Parse}));
+
                 var streamWriter = new StreamWriter(outputFile.OpenWrite());
-                streamWriter.Write(kmlString);
+                streamWriter.Write("test");
                 streamWriter.Close();
+
+                bool IsValidArguments(FileInfo? inFile, ref FileInfo? outFile,
+                    out string validationErr)
+                {
+                    if (inFile == null)
+                    {
+                        validationErr = "You must specify an input file.";
+                        return false;
+                    }
+
+                    if (!inFile.Exists)
+                    {
+                        validationErr = "Input file doesn't exist.";
+                        return false;
+                    }
+                    
+                    // If output file is not specified – set name to the
+                    // name of an input file with .kml extension
+                    if (outFile == null)
+                    {
+                        outFile = new FileInfo(Path.Join(inFile.DirectoryName,
+                            $"{inFile.Name.Substring(0, inFile.Name.IndexOf(".", StringComparison.Ordinal))}.kml"));
+                    }
+                    
+                    if (!Directory.Exists(outFile.DirectoryName))
+                    {
+                        validationErr = "Output directory doesn't exist.";
+                        return false;
+                    }
+
+                    // If output file with the same name already exists –
+                    // prompt user to change a name of the file
+                    while (outFile.Exists)
+                    {
+                        Console.Write("Output file is already exists. Do you want to overwrite it? [y/N] ");
+                        var opt = Console.ReadLine();
+
+                        if (String.IsNullOrEmpty(opt) || opt.ToLower() == "no" ||
+                            opt.ToLower() == "n")
+                        {
+                            Console.Write("Enter a new name: ");
+                            var name = Console.ReadLine();
+                            outFile = new FileInfo(
+                                Path.Join(outFile.DirectoryName, name));
+                            continue;
+                        }
+                        
+                        if (opt.ToLower() == "yes" ||
+                            opt.ToLower() == "y")
+                        {
+                            break;
+                        }
+                    }
+                    
+                    validationErr = string.Empty;
+                    return true;
+                }
             },
             inputOption, outputOption);
 
         return await rootCommand.InvokeAsync(args);
-        
-        string GenerateKml(detectionrun detectionRun, string name)
-        {
-            var sb = new StringBuilder();
-
-            sb.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-            sb.Append("\n<kml xmlns=\"http://www.opengis.net/kml/2.2\">");
-            sb.Append("\n  <Document>");
-            sb.Append($"\n    <name>Capture file: {name}</name>" +
-                      $"\n    <description>Started on {detectionRun.starttime}." +
-                      $"\nCaptured {detectionRun.wirelessnetwork.Count(wn => wn.type != "probe")} AP(s) and {detectionRun.wirelessnetwork.Count(wn => wn.type == "probe")} client(s)</description>");
-
-            foreach (var wn in detectionRun.wirelessnetwork)
-            {
-                // If wireless network is a client – skip
-                if (wn.type == "probe")
-                {
-                    continue;
-                }
-                
-                sb.Append("\n    <Placemark>");
-
-                sb.Append($"\n      <name>{wn.SSID.First().essid.First().Value}</name>.");
-                sb.Append($"\n      <description>Manufacturer: {wn.manuf}." +
-                          $"\n        Last update: {wn.lasttime}." +
-                          $"\n        Channel: {wn.channel}." +
-                          $"\n        BSSID: {wn.BSSID}." +
-                          $"\n        Frequency {wn.freqmhz}." +
-                          $"\n      </description>");
-                sb.Append($"\n      <Point>" +
-                          $"\n        <coordinates>{wn.gpsinfo.First().peaklon},{wn.gpsinfo.First().peaklat},{wn.gpsinfo.First().peakalt}</coordinates>" +
-                          $"\n      </Point>");
-            
-                sb.Append("\n    </Placemark>");
-            }
-
-            sb.Append("\n  </Document>");
-            sb.Append("\n</kml>");
-        
-            return sb.ToString();
-        }
     }
 }
